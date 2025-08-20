2eff9ae9089dcd176043e4160d618dfafeeb216d
HarshitaJuyal <h.karthiknair@gmail.com>
2025-08-20 20:43:06 +0530

    Add Observer Pattern Code

---
+++ Behavioural Design Pattern/2_Observer_Pattern/1_observer_pattern.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+// REMEMBER: Abstract Class -> interface
+//           Concrete Class -> class which inherits interface
+
+// IClassName is like a convention of indicating that this is an abstract class (Interface)
+class ISubscriber {
+public:
+    virtual void update() = 0;
+    virtual ~ISubscriber() {}  // virtual destructor for interface
+};
+
+// this is the observable class. i.e. observer classes can observe observable class.
+// Abstract Observable interface: a YouTube channel interface
+class IChannel {
+public:
+    virtual void subscribe(ISubscriber* subscriber) = 0;
+    virtual void unsubscribe(ISubscriber* subscriber) = 0;
+    virtual void notifySubscribers() = 0;
+    virtual ~IChannel() {}
+};
+
+
+// Concrete Subject: a YouTube channel that observers can subscribe to
+class Channel : public IChannel {
+private:
+    vector<ISubscriber*> subscribers;  // list of subscribers
+    string name; // name of the youtube channel
+    string latestVideo;               // latest uploaded video title
+public:
+    Channel(const string& name) {
+        this->name = name;
+    }
+
+    // Add a subscriber (avoid duplicates)
+    void subscribe(ISubscriber* subscriber) override {
+
+        // find pointer wil reach subscribers.end() if there is no given subscriber in the vector
+        // thus we can avoid adding duplicate subscribers
+        if (find(subscribers.begin(), subscribers.end(), subscriber) == subscribers.end()) {
+            subscribers.push_back(subscriber);
+        }
+    }
+
+    // Remove a subscriber if present
+    void unsubscribe(ISubscriber* subscriber) override {
+        auto it = find(subscribers.begin(), subscribers.end(), subscriber);
+        if (it != subscribers.end()) {
+            subscribers.erase(it);
+        }
+    }
+
+    // Notify all subscribers of the latest video when a new video is added
+    void notifySubscribers() override {
+        for (ISubscriber* sub : subscribers) {
+            sub->update();
+        }
+    }
+
+    // Upload a new video and notify all subscribers
+    void uploadVideo(const string& title) {
+        latestVideo = title;
+        cout << "\n[" << name << " uploaded \"" << title << "\"]\n";
+        notifySubscribers();
+    }
+
+    // Read video data
+    string getVideoData() {
+        return "\nCheckout our new Video : " + latestVideo + "\n";
+    }
+};
+
+// Concrete Observer: represents a subscriber to the channel
+class Subscriber : public ISubscriber {
+private:
+    string name;
+    Channel* channel;
+public:
+    Subscriber(const string& name, Channel* channel) {
+        this->name = name;
+        this->channel = channel;
+    }
+
+    // Called by Channel; prints notification message
+    void update() override {
+        cout << "Hey " << name << "," << this->channel->getVideoData();
+    }
+};
+
+int main() {
+    // Create a channel and subscribers
+    Channel* channel = new Channel("CraftedByKarthik");
+
+    Subscriber* subs1 = new Subscriber("Varun", channel);
+    Subscriber* subs2 = new Subscriber("Tarun", channel);
+
+    // Varun and Tarun subscribe to CoderArmy
+    channel->subscribe(subs1);
+    channel->subscribe(subs2);
+
+    // Upload a video: both Varun and Tarun are notified
+    channel->uploadVideo("Moonlight Sonata movement 1 Tutorial ");
+
+    // Varun unsubscribes; Tarun remains subscribed
+    channel->unsubscribe(subs1);
+
+    // Upload another video: only Tarun is notified
+    channel->uploadVideo("Decorator Pattern Tutorial");
+
+    return 0;
+}
